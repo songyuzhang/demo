e9e6dcc864aef9ca6900cf38e05d3e5937eb254d
songyuzhang <songyuzhang@hotmail.com>
2018-12-27 14:30:04 +0800

    新建版本

---
+++ src/main/java/com/example/demo/Application.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import com.alibaba.dubbo.spring.boot.annotation.EnableDubboConfiguration;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableDubboConfiguration
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
+
