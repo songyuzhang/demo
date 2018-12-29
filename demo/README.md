667fe94aeedce27b2d09a20cef1f9b73fb088877
songyuzhang <songyuzhang@hotmail.com>
2018-12-29 16:39:38 +0800

    renew project

--- demo/README.md
+++
@@ -1,4 +0,0 @@
-# Demo
-**SpringBoot demo**
-
-已整合mysql mybatis redis rabbitmq dubbo
--- demo/spring-webmvc-demo/src/main/java/com/example/demo/rabbitmq/service/RabbitmqService.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.rabbitmq.service;
-
-import com.example.demo.vo.DemoVO;
-
-public interface RabbitmqService {
-
-    String sendDemo(DemoVO demoVO);
-}
